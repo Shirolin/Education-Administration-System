1bb2d75d278c5f0282e8995f96c2ee92883fc407
Shirolin <shirolin.com@gmail.com>
2025-01-09 17:46:59 +0800

    ✨ feat(models): 添加课程、账单及相关模型

---
+++ app/Models/Course/SubCourse.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Course;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubCourse extends Model
+{
+    use HasFactory;
+
+    protected $table = 'sub_courses';
+}
---
+++ app/Models/Invoice/InvoiceItem.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Invoice;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceItem extends Model
+{
+    use HasFactory;
+
+    protected $table = 'invoice_items';
+}
