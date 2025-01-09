eb7956f6bea87af77c01f88454cfe4977f7353c4
Shirolin <shirolin.com@gmail.com>
2025-01-09 14:37:36 +0800

    ✨ feat(exception): 增强异常处理以支持JSON响应格式

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,18 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponseTrait;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponseTrait;
+
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +33,32 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($request->expectsJson()) {
+            if ($exception instanceof ValidationException) {
+                return $this->error($exception->validator->errors(), null, 422);
+            }
+            if ($exception instanceof NotFoundHttpException) {
+                return $this->error('资源未找到！', null, 404);
+            }
+            if ($exception instanceof MethodNotAllowedHttpException) {
+                return $this->error('请求方法不允许！', null, 405);
+            }
+            if ($exception instanceof AuthenticationException) {
+                return $this->error('未登录，请先登录！', null, 401);
+            }
+
+            return $this->error('服务器错误', null, 500);
+        }
+
+        return parent::render($request, $exception);
     }
 }
