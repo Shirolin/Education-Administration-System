f23163fda92c18426a61cc2de6e649f27a19efdc
Shirolin <shirolin.com@gmail.com>
2025-01-11 20:46:21 +0800

    ✨ feat(test): 添加支付处理功能及异常日志记录

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Role\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use OmiseAccount;
+use OmiseCharge;
+use Predis\Command\Redis\FUNCTIONS;
 
 class TestController extends ApiController
 {
@@ -15,7 +18,57 @@ class TestController extends ApiController
 
     public function test(Request $request)
     {
+        return $this->success($this->charge(10000, 'jpy', '112233'));
         $user = $request->user();
+
+        return $this->success($this->getChargeTestData());
+        $publickey = env('OMISE_PUBLIC_KEY');
+        $secretkey = env('OMISE_SECRET_KEY');
+        $account = OmiseAccount::retrieve($publickey, $secretkey);
+
+        $user = $account; // your email will be printed on a screen.
+        Log::info(json_encode($user));
+
+        dd($user);
+        return;
+
+        $charge = OmiseCharge::create(array(
+            'amount' => 10025,
+            'currency' => 'jpy',
+            'card' => '4445566'
+        ));
+
+        echo ($charge['status']);
+
+        print('<pre>');
+        print_r($charge);
+        print('</pre>');
+
+        dd($charge);
+
         return $this->success($user);
+    }
+
+    private function charge($amount, $currency, $card)
+    {
+        try {
+            $charge = OmiseCharge::create(array(
+                'amount' => $amount,
+                'currency' => $currency,
+                'card' => $card
+            ), env('OMISE_PUBLIC_KEY'), env('OMISE_SECRET_KEY'));
+            $result = [
+                'id' => $charge['id'],
+                'amount' => $charge['amount'],
+                'currency' => $charge['currency'],
+                'card' => $charge['card'],
+            ];
+            Log::info('支付成功', ['charge' => $charge->toArray(), 'result' => $result]);
+        } catch (\Exception $e) {
+            Log::error('支付失败', ['code' => $e->getCode(), 'message' => $e->getMessage()]);
+            throw new \Exception('支付失败');
+        }
+
+        return $charge;
     }
 }
