54e4ef1f55a65caa4b8681be27a4d6572710cec0
Shirolin <shirolin.com@gmail.com>
2025-01-11 15:05:41 +0800

    ✨ feat(service): 添加获取当前用户及用户ID的方法

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -8,4 +8,20 @@ class BaseService
      * 默认每页显示数量
      */
     const DEFAULT_PER_PAGE = 10;
+
+    /**
+     * 获取当前用户
+     */
+    protected function user()
+    {
+        return auth('api')->user();
+    }
+
+    /**
+     * 获取当前用户ID
+     */
+    protected function userId()
+    {
+        return auth('api')->id();
+    }
 }
